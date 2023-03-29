f049a21372c1fea8748ae80ae40aad02fc551940
JaeWon <bjw1622@gmail.com>
2023-03-29 17:26:09 +0900

    feat : error page

--- clover/pages/404.js
+++ clover/pages/404.js
@@ -8,22 +8,21 @@ const notFound = () => {
   const router = useRouter();
 
   return (
-    <div id={styles.errorImg}>
-      <Image src={logo} height={298} width={379} alt="logo" />
-      <div id={styles.textBox}>
-        <div id={styles.text}>미지의 공간으로 와버렸네요!</div>
-        <div id={styles.text}>
-          다시 해빗 페이지로 돌아갈 수 있게 도와드릴게요
-        </div>
+    <div id={styles.errorDiv}>
+      <div id={styles.errorImg}>
+        <Image src={logo} height={298} width={379} alt="logo" />
       </div>
-
-      <Link
-        href={"/"}
-        style={{ textDecoration: "none" }}
-        className={router.pathname === "/" ? "active" : ""}
-      >
-        <button>메인으로 돌아가기</button>
-      </Link>
+      <div id={styles.text}>미지의 공간으로 와버렸네요!</div>
+      <div id={styles.text}>다시 해빗 페이지로 돌아갈 수 있게 도와드릴게요</div>
+      <div id={styles.main}>
+        <Link
+          href={"/"}
+          style={{ textDecoration: "none" }}
+          className={router.pathname === "/" ? "active" : ""}
+        >
+          <button id={styles.mainBtn}>메인으로 돌아가기</button>
+        </Link>
+      </div>
     </div>
   );
 };
