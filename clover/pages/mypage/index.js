69c55501dc76c28a8c561540d7e921d6704e0137
JaeWon <bjw1622@gmail.com>
2023-03-31 20:19:48 +0900

    feat : mypage 추가

--- clover/pages/mypage/index.js
+++ clover/pages/mypage/index.js
@@ -1,15 +1,19 @@
 import styles from "./index.module.scss";
 import Image from "next/image";
 import logo from "../../public/images/avatar_default.png";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
 const MyPage = () => {
+  const router = useRouter();
+
   return (
     <div id={styles.mypage}>
       <div id={styles.mypageTitle}>마이페이지</div>
       <Image src={logo} height={130} width={130} alt="logo" />
       <div id={styles.email}>
         <div id={styles.emailText}>이메일</div>
-        <input id={styles.emailInput} placeholder="bjw1622@gmail.com"></input>
+        <input id={styles.emailInput} placeholdesr="bjw1622@gmail.com"></input>
       </div>
       <div id={styles.nickname}>
         <div id={styles.nicknameText}>닉네임</div>
@@ -19,7 +23,13 @@ const MyPage = () => {
         </div>
       </div>
       <button id={styles.logout}>로그아웃</button>
-      <button id={styles.exit}>탈퇴하기</button>
+      <Link
+        href={"/withdraw"}
+        style={{ textDecoration: "none" }}
+        className={router.pathname === "/" ? styles.active : ""}
+      >
+        <button id={styles.exit}>탈퇴하기</button>
+      </Link>
     </div>
   );
 };
