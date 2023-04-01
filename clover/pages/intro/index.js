cc6278c322a1db00d7cfb2049d2b37bc070b1d6d
JaeWon <bjw1622@gmail.com>
2023-04-01 20:34:40 +0900

    intro 추가

--- clover/pages/intro/index.js
+++ clover/pages/intro/index.js
@@ -4,6 +4,10 @@ import logo from "../../public/images/HABIT_ HAVE-IT.png";
 import Link from "next/link";
 import { useRouter } from "next/router";
 import Vector from "../../public/images/Vector.png";
+import Mask1 from "../../public/images/Mask group_1.png";
+import Mask2 from "../../public/images/Mask group_2.png";
+import Mask3 from "../../public/images/Mask group_3.png";
+import Mask4 from "../../public/images/Mask group_4.png";
 
 const index = () => {
   const router = useRouter();
@@ -61,6 +65,60 @@ const index = () => {
         <div id={styles.subtitle}>
           손쉬운 습관 체크와 회고 작성을 통해 더 나은 내가 될 수 있도록
           도와드려요.
+        </div>
+        <div>
+          <Image
+            style={{ marginTop: "72px", marginRight: "12px" }}
+            src={Mask1}
+            height={500}
+            width={380}
+            alt="logo"
+          />
+          <Image
+            style={{ marginTop: "72px", marginLeft: "12px" }}
+            src={Mask2}
+            height={500}
+            width={776}
+            alt="logo"
+          />
+        </div>
+        <div>
+          <Image
+            style={{ marginTop: "24px", marginRight: "12px" }}
+            src={Mask4}
+            height={500}
+            width={776}
+            alt="logo"
+          />
+          <Image
+            style={{ marginTop: "24px", marginLeft: "12px" }}
+            src={Mask3}
+            height={500}
+            width={380}
+            alt="logo"
+          />
+        </div>
+      </div>
+      <div id={styles.third}>
+        <div id={styles.thirdtitle}>HAVE IT NOW</div>
+        <div id={styles.thirdsubtitle}>
+          지금 바로 해비터즈와 함께 습관 형성을 시작해보세요.
+        </div>
+        <div id={styles.thirdbtns}>
+          <Link
+            href={"/signin"}
+            style={{ textDecoration: "none" }}
+            id={styles.signinbtn}
+          >
+            로그인하기
+          </Link>
+          <Link
+            href={"/signup"}
+            style={{ textDecoration: "none" }}
+            id={styles.signupbtn}
+          >
+            회원가입하기
+          </Link>
         </div>
       </div>
     </div>
