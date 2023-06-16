e1309e948c554af274e8ee894d0cf206a6ee7317
kwonjoosung <kwonjoosung@gmail.com>
2023-06-16 20:48:38 +0900

    feat: Like 기본 구조 추가

---
+++ src/main/java/com/clover/habbittracker/domain/like/api/LikeController.java
@@ -0,0 +1,12 @@
+package com.clover.habbittracker.domain.like.api;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import lombok.RequiredArgsConstructor;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/likes")
+public class LikeController {
+}
